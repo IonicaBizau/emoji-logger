be928d678b5b8207c664db97c2a0adac2fa44df7
Ionică Bizău <bizauionica@gmail.com>
2015-12-13 11:11:00 +0200

    Added tests

---
+++ test/index.js
@@ -0,0 +1,32 @@
+const tester = require("tester")
+    , log = require("../lib")
+    ;
+
+tester.describe("errors", test => {
+
+    // log(...)
+    test.it("should throw NO_SUCH_LOGGER_TYPE when the type is invalid.", () => {
+        test.expect(() => {
+            log("foo", "does-not-exist");
+        }).toThrow(Error, "There is no such logger type: doesn-not-exist");
+    });
+
+    // log.add(...)
+    test.it("should throw NO_SUCH_ICON when the icon is invalid.", () => {
+        test.expect(() => {
+            log.add("foo", "does-not-exist");
+        }).toThrow(Error, "There is no such icon: doesn-not-exist");
+    });
+});
+
+tester.describe("streams", test => {
+    test.it("should support custom streams", () => {
+        log("test", "info", {
+            stream: {
+                write: (msg) => {
+                    test.expect(msg).toBe('  \x1b[38;5;68mℹ️ test\x1b[39m\n' );
+                }
+            }
+        });
+    });
+});
